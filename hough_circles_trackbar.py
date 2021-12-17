74ff21a5ed63d5cd6d0267779f6758a89df462aa
Alexander <you@example.com>
2021-12-17 21:03:01 +0300

    222

--- hough_circles_trackbar.py
+++ hough_circles_trackbar.py
@@ -4,12 +4,12 @@ import numpy as np
 def nothing(x): pass
 cv2.namedWindow('image')
 
-img_path = 'apple.jpg'
+# img_path = 'apple.jpg'
+img_path = 'photo_2021-12-17_20-42-08.jpg'
 
 cimg = cv2.imread(img_path)
 gimg = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY)
 
-# Create trackbars for color change
 # Hue is from 0-179 for Opencv
 cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
 cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
@@ -24,7 +24,7 @@ cv2.createTrackbar('Param 1','image', 1, 500, nothing)
 cv2.createTrackbar('Param 2','image', 1, 100, nothing)
 cv2.createTrackbar('Min dist','image', 1, 1000, nothing)
 cv2.createTrackbar('Min rad','image', 1, 100, nothing)
-cv2.createTrackbar('Max rad','image', 1, 1000, nothing)
+cv2.createTrackbar('Max rad','image', 1, 100, nothing)
 
 cv2.setTrackbarPos('HMin', 'image', 140)
 cv2.setTrackbarPos('HMax', 'image', 5)
@@ -36,9 +36,6 @@ cv2.setTrackbarPos('Param 2','image', 33)
 cv2.setTrackbarPos('Min dist','image', 180)
 cv2.setTrackbarPos('Min rad','image', 7)
 cv2.setTrackbarPos('Max rad','image', 133)
-
-hMin = sMin = vMin = hMax = sMax = vMax = 0
-phMin = psMin = pvMin = phMax = psMax = pvMax = 0
 
 while True:
     hMin = cv2.getTrackbarPos('HMin', 'image')
@@ -83,15 +80,17 @@ while True:
                         param1, # Canny high threshold
                         param2, # minimum number of votes
                         minRadius=min_radius,
-                        maxRadius=max_radius
+                        maxRadius=max(min_radius, max_radius)
     )
 
     circles = np.uint16(np.around(circles))
     cimg2 = cv2.bitwise_and(cimg, cimg, mask=mask)
     cimg2[result==0] = 255
     for i in circles[0,:]:
-        cv2.circle(cimg2,(i[0],i[1]),i[2],(0,255,0),2)
-        cv2.circle(cimg2,(i[0],i[1]),2,(0,0,255),3)
+        if len(i.shape)==0:
+            continue
+        cv2.circle(cimg2,(i[0],i[1]),i[2],(0,255,0),1)
+        cv2.circle(cimg2,(i[0],i[1]),2,(0,255,255),1)
 
     cv2.imshow('image', cv2.resize(cimg2,[1200,600]))
     if cv2.waitKey(1) & 0xFF == ord('q'):

4794ec40f42b462e35c18b1cac933d679755c5c6
Alexander <you@example.com>
2021-12-18 20:40:17 +0300

    cleanup

--- train.py
+++ train.py
@@ -10,43 +10,24 @@ import torch
 import torch.utils.data
 from apple_dataset import AppleDataset
 
-# def get_transform(train):
-#     transforms = []
-#     transforms.append(T.ToTensor())
-#     if train:
-#         transforms.append(T.RandomHorizontalFlip(0.5))
-#     return T.Compose(transforms)
-
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-def get_dataset():
-    device = torch.device('cpu')
-    data_dir = 'MiniApples/detection/train'
-
+def get_dataset(data_dir):
+    # device = torch.device('cpu')
     dataset = AppleDataset(data_dir, None)
     return dataset
 
 def train_all():
-    dataset = get_dataset()
+    dataset = get_dataset('MiniApples/detection/train')
     # data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1, collate_fn=collate_fn)
     scores = []
-    # i = 0
     for img, mask in tq.tqdm(dataset):
         mask[mask>0] = 255
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         # detect_and_show(img)
-
         s = detect_and_score(img, mask)
         scores.append(s)
-        # i += 1
-        # if i>100:
-        #     break
-
-        # im_show(image)
-        # targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        # im_id = targets[0]['image_id'].item()
-        # im_name = data_loader.dataset.get_img_name(im_id)
     return np.array(scores).mean()
 
 def main():
